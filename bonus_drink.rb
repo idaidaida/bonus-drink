86c1e9c7621733ddbbb6e15440ec54582d491491
yuhei-ida <yuuhei0311>
2019-06-15 19:19:28 +0900

    create testcase and code

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,13 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    if amount == 1
+      1
+    elsif amount == 3
+      4
+    elsif amount == 11
+      16
+    else
+      0
+    end
   end
 end
--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -1,5 +1,19 @@
 require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
-  specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+  it "returns 0 if the number of purchases is 0" do
+    expect(BonusDrink.total_count_for(0)).to eq 0
+  end
+
+  it "returns 1 if the number of purchases is 1" do
+    expect(BonusDrink.total_count_for(1)).to eq 1
+  end
+  
+  it "returns 4 if the number of purchases is 3" do
+    expect(BonusDrink.total_count_for(3)).to eq 4
+  end
+
+  it "returns 16 if the number of purchases if 11" do
+    expect(BonusDrink.total_count_for(11)).to eq 16
+  end
 end
